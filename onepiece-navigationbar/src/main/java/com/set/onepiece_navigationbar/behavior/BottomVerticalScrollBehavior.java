fc0bc45dbd227fc2217f6a97e26271dcfc7c2862
LeoCheung0221 <LeoCheung0221>
2016-06-08 09:58:36 +0800

    commit library

---
+++ onepiece-navigationbar/src/main/java/com/set/onepiece_navigationbar/behavior/BottomVerticalScrollBehavior.java
@@ -0,0 +1,91 @@
+package com.set.onepiece_navigationbar.behavior;
+
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPropertyAnimatorCompat;
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
+import android.view.View;
+import android.view.animation.Interpolator;
+
+/**
+ * author：leo on 2016/6/7 17:16
+ * email： leocheung4ever@gmail.com
+ * description: Vertical behavior of Scrolling
+ * what & why is modified:
+ */
+public class BottomVerticalScrollBehavior<V extends View> extends VerticalScrollingBehavior<V> {
+
+    private static final Interpolator INTERPOLATOR = new LinearOutSlowInInterpolator();
+    private int mBottomNavHeight;
+    private int mDefaultOffset;
+//    private WeakReference<V> mViewRef;
+
+    private ViewPropertyAnimatorCompat mTranslationAnimator;
+    private boolean hidden = false;
+
+    @Override
+    public boolean onLayoutChild(CoordinatorLayout parent, final V child, int layoutDirection) {
+        // First let the parent lay it out
+        parent.onLayoutChild(child, layoutDirection);
+//        mViewRef = new WeakReference<>(child);
+
+        child.post(new Runnable() {
+            @Override
+            public void run() {
+                mBottomNavHeight = child.getHeight();
+            }
+        });
+
+        mDefaultOffset = 0;
+
+        return super.onLayoutChild(parent, child, layoutDirection);
+    }
+
+    @Override
+    public void onNestedVerticalScrollUnconsumed(CoordinatorLayout coordinatorLayout, V child, @ScrollDirection int scrollDirection, int currentOverScroll, int totalScroll) {
+        // Empty body
+    }
+
+    @Override
+    public void onNestedVerticalPreScroll(CoordinatorLayout coordinatorLayout, V child, View target, int dx, int dy, int[] consumed, @ScrollDirection int scrollDirection) {
+//        handleDirection(child, scrollDirection);
+    }
+
+    @Override
+    public void onNestedVerticalScrollConsumed(CoordinatorLayout coordinatorLayout, V child, @ScrollDirection int scrollDirection, int currentOverScroll, int totalConsumedScroll) {
+        handleDirection(child, scrollDirection);
+    }
+
+    private void handleDirection(V child, int scrollDirection) {
+        if (scrollDirection == ScrollDirection.SCROLL_DIRECTION_DOWN && hidden) {
+            hidden = false;
+            animateOffset(child, mDefaultOffset);
+        } else if (scrollDirection == ScrollDirection.SCROLL_DIRECTION_UP && !hidden) {
+            hidden = true;
+            animateOffset(child, mBottomNavHeight + mDefaultOffset);
+        }
+    }
+
+    @Override
+    protected boolean onNestedDirectionFling(CoordinatorLayout coordinatorLayout, V child, View target, float velocityX, float velocityY, boolean consumed, @ScrollDirection int scrollDirection) {
+        if (consumed) {
+            handleDirection(child, scrollDirection);
+        }
+        return consumed;
+    }
+
+    private void animateOffset(final V child, final int offset) {
+        ensureOrCancelAnimator(child);
+        mTranslationAnimator.translationY(offset).start();
+    }
+
+    private void ensureOrCancelAnimator(V child) {
+        if (mTranslationAnimator == null) {
+            mTranslationAnimator = ViewCompat.animate(child);
+            mTranslationAnimator.setDuration(400);
+            mTranslationAnimator.setInterpolator(INTERPOLATOR);
+        } else {
+            mTranslationAnimator.cancel();
+        }
+    }
+}
